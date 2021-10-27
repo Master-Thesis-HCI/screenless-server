515517b18e306d6e2101caa120d2e41a3e6fda8e
Roman Peters <mail@romanpeters.nl>
2021-10-27 13:34:10 +0200

    Fix webserver

--- app/api.py
+++ app/api.py
@@ -6,6 +6,7 @@ from app import database as db
 from app import app
 
 
+@app.route("/api", methods=['GET'])
 @app.route("/api/", methods=['GET'])
 def endpoints():
     """Overview of the API endpoints."""
@@ -19,6 +20,7 @@ def device_info(entry: db.Devices) -> dict:
     return result
 
 
+@app.route("/api/<string:device_id>", methods=['GET'])
 @app.route("/api/<string:device_id>/", methods=['GET'])
 def get_screentime(device_id):
     """Get device screentime information"""
@@ -30,33 +32,39 @@ def get_screentime(device_id):
     return jsonify(device_info(device_entry))
 
 
+@app.route("/api/<string:device_id>", methods=['POST'])
 @app.route("/api/<string:device_id>/", methods=['POST'])
 def set_screentime(device_id):
     """Set device screentime information"""
+    print(request)
     print(request.json)
-    if not request.json or 'screentime' not in request.json:
-        return abort(400)
-    updated = datetime.datetime.now()
-    session = db.Session()
-    device_entry = db.Devices(device_id=device_id,
-                              screentime=request.json['screentime'],
-                              updated=updated)
-    session.merge(device_entry)
-    session.commit()
-    return jsonify(device_info(device_entry))
+    print(request.data)
+
+
+    #if not request.json or 'screentime' not in request.json:
+    #    return abort(400)
+    #updated = datetime.datetime.now()
+    #session = db.Session()
+    #device_entry = db.Devices(device_id=device_id,
+    #                          screentime=request.json['screentime'],
+    #                          updated=updated)
+    #session.merge(device_entry)
+    #session.commit()
+    #return jsonify(device_info(device_entry))
+    return jsonify({"success": True}
 
 
 endpoints = [{"path": "/api/",
               "name": endpoints.__name__,
               "method": "GET",
               "description": endpoints.__doc__},
-             {"path": "/api/<string:device_id>/",
+             {"path": "/api/{string:device_id}/",
               "name": get_screentime.__name__,
               "method": "GET",
               "description": get_screentime.__doc__},
-             {"path": "/api/<string:device_id>/",
-              "name": get_screentime.__name__,
+             {"path": "/api/{string:device_id}/",
+              "name": set_screentime.__name__,
               "method": "POST",
-              "description": get_screentime.__doc__},
+              "description": set_screentime.__doc__},
              ]
 
